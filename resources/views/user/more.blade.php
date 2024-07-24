51ed7f7b709c0cedbf9c8a27c9fb40e61b97ae71
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-07-24 11:40:17 +0530

    fghjkl

--- resources/views/user/more.blade.php
+++ resources/views/user/more.blade.php
@@ -291,21 +291,21 @@
                     </div>
                 </div>
                 <ul class="py-20 px-16 box-service line">
-                    <li><a href="#" class="item">
+                    <li><a href="{{route('user.Withdraw')}}" class="item">
                         <div class="box-icon w-48 round"><span class="icon icon-import"></span></div>
-                        <span class="fw-6 text-dark">Receive</span>
+                        <span class="fw-6 text-dark">Withdrawal</span>
                     </a></li>
-                    <li><a href="#" class="item">
+                    <li><a href="{{route('user.invest')}}" class="item">
                         <div class="box-icon w-48 round"><span class="icon icon-bag-2"></span></div>
-                        <span class="fw-6 text-dark">Buy</span>
+                        <span class="fw-6 text-dark">Deposit</span>
                     </a></li>
                     <li><a href="#" class="item">
                         <div class="box-icon w-48 round"><span class="icon icon-send-2"></span></div>
-                        <span class="fw-6 text-dark">Send</span>
+                        <span class="fw-6 text-dark">Invite</span>
                     </a></li>
                     <li><a href="#" class="item">
                         <div class="box-icon w-48 round"><span class="icon icon-arrow-swap"></span></div>
-                        <span class="fw-6 text-dark">Swap</span>
+                        <span class="fw-6 text-dark">My Order</span>
                     </a></li>
                 </ul>
                 <div class="mt-24">
