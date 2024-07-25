b2cb19e2f7a96008980e0d2a4050ff28d1373517
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-07-25 12:05:53 +0530

    dgh

--- resources/views/user/invest/confirmDeposit.blade.php
+++ resources/views/user/invest/confirmDeposit.blade.php
@@ -125,8 +125,29 @@
                     <div data-v-56930b78="" title="{{$walletAddress}}">
                         <canvas width="50" height="50" style="display: none;"></canvas>
                         <img alt="Scan me!" src="{{$qr_code}}" style="display: block;">
-                        <p style="margin-top: 10px; text-align: center;">{{$walletAddress}}</p>
+                        <!-- <p style="margin-top: 10px; text-align: center;">{{$walletAddress}}</p> -->
                     </div>
+                    <div class="pb-24 mb-24 line">
+                <p>@lang('Deposit Address')</p>
+                <div class="mb-16 box-ip-field-val">
+
+                    <img class="lazyload" data-src="images/banner/banner-nft-3.jpg" alt="img">
+                    <input type="text" class="form-control" value="{{$walletAddress}}">
+                    <div class="group-icon">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"  style="font-size:24px; color:#fff" type="button" class="copy-btn" onclick="copyFunctionwallet()" >
+                            <g opacity="0.6">
+                            <path d="M14.1666 11.1673V13.6673C14.1666 17.0007 12.8333 18.334 9.49996 18.334H6.33329C2.99996 18.334 1.66663 17.0007 1.66663 13.6673V10.5007C1.66663 7.16732 2.99996 5.83398 6.33329 5.83398H8.83329" stroke="#1A1528" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M14.1663 11.1673H11.4997C9.49967 11.1673 8.83301 10.5007 8.83301 8.50065V5.83398L14.1663 11.1673Z" stroke="#1A1528" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M9.66638 1.66602H12.9997" stroke="#1A1528" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5.83337 4.16602C5.83337 2.78268 6.95004 1.66602 8.33337 1.66602H10.5167" stroke="#1A1528" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18.3337 6.66602V11.8243C18.3337 13.116 17.2837 14.166 15.9921 14.166" stroke="#1A1528" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18.3334 6.66602H15.8334C13.9584 6.66602 13.3334 6.04102 13.3334 4.16602V1.66602L18.3334 6.66602Z" stroke="#1A1528" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </g>
+                        </svg>
+                       
+                               
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
@@ -134,6 +155,77 @@
     <div class="btn-fixed">
         <a href="{{route('user.invest')}}" class="tf-btn primary" style="width:1000px;">CANCEL</a>
     </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script>
+        
+        
+        
+        function storeTransaction() {
+            
+                var orderId = $('#orderId').val();
+        
+        $.ajax({
+                    type: "post",
+                    url: "https://qstaig.com/user/checkPaymentStatus",
+                    data: {"orderId":orderId,"_token": "{{ csrf_token() }}"},
+                    success: function (response) 
+                    {
+                     console.log(response);  
+                     
+                     if(response==1)
+                     {
+                      
+                      iziToast.success({
+            			message: 'Deposit Successfully',
+            			position: "topRight"
+            		});
+             
+                      window.setTimeout(function() {
+                        window.location.href = 'https://qstaig.com/user/dashboard';
+                    }, 3000);
+        
+                     }
+                    }
+                    });
+          
+        }
+        
+        // Set the interval to 5 minutes (300,000 milliseconds)
+        const interval = 2000;
+        
+        // Call the function immediately and then at the specified interval
+        storeTransaction(); // Call the function immediately
+        setInterval(storeTransaction, interval);
+        
+        </script>
+        <script>
+        function copyFunctionwallet() {
+    
+            console.time('time1');
+           var temp = $("<input>");
+              $("body").append(temp);
+            temp.val($('#wallet_address1').text()).select();
+            document.execCommand("copy");
+            temp.remove();
+             console.timeEnd('time1');
+                
+             iziToast.success({
+            message: 'Copied',
+            position: "topRight"
+           });
+
+
+            //navigator.clipboard.writeText(copyText.value);
+
+            /* Alert the copied text */
+            //   alert("copied: "+copyText.value)
+            }
+        
+        </script>
 
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jquery.min.js"></script>
