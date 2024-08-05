785d04f0df2a525ea9650730536839c382eda52e
Sachin <prajapatsachin835@gmail.com>
2024-08-05 18:13:51 +0530

    ded

--- app/Http/Controllers/UserPanel/Dashboard.php
+++ app/Http/Controllers/UserPanel/Dashboard.php
@@ -151,10 +151,10 @@ $this->data['nftsLatest'] = $filteredNftsLatest;
         $user=Auth::user();
         $startOfDay = Carbon::today();
         $endOfDay = Carbon::tomorrow()->subSecond();
-        
+
         $activity = Activity::where('user_id', $user->id)
-                  //  ->whereBetween('created_at', [$startOfDay, $endOfDay])
-                  //  ->latest('created_at')
+                   ->whereBetween('created_at', [$startOfDay, $endOfDay])
+                   ->latest('created_at')
                    ->get()?? 0;
 
         $this->data['activity'] = $activity;
